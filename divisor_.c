adeb77c99e4e1bd8fae86cfd14b664cd55c45971
JangHongJoon <wkdghdwns19969@gmail.com>
2018-09-05 19:19:11 +0900

    2018.09.06_Desktop_DB

--- divisor_.c
+++ divisor_.c
@@ -9,11 +9,10 @@ int main(){
 	scanf("%d",&a);
 	
 	for (i=1; i*i<=a; i++){
-		if (a%i==0) c[p++]=i;
-	}
-	
-	for (i=0; i<p; i++){
-		printf("%d ",c[i]);
+		if (a%i==0) {
+			printf("%d ",i);
+			c[p++]=i;
+		}
 	}
 	
 	int sw=0;
--- highest_divisor.c
+++ highest_divisor.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+int c[100000];
+
 int main(){
-	int m,n,r=1;
+	int m,n,max=1,p=0;
 	int i;
 	
 	scanf("%d %d",&m,&n);
 	
-	if (m<n) {int t; t=n; n=m; m=t;}
+	if (m<n) {
+		int t; 
+		t=n; 
+		n=m; 
+		m=t;	
+	}
 	
 	for (i=1; i*i<=m; i++){
-		if (m%i==0 && n%i==0) r*=i;
+		if (m%i==0 && n%i==0) {
+			c[p++]=i;
+		}
 	}
-	printf("%d\n",r);
+	
+	int sw=0;
+	for (i=p-1; i>=0; i--){
+		int im=m/c[i];
+		if (m%im==0 && n%im==0) {
+			printf("%d ",i);
+			max*=i;
+		}
+	}
+	
+	printf("\n%d\n",max);
 } 
